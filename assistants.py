db5b821a4ec96289dd9aff183a7249ebaa3e3fc0
aquifer-joseph-miller <joseph.miller@aquifer.org>
2025-05-27 21:55:48 -0400

    Add files via upload

--- assistants.py
+++ assistants.py
@@ -2,6 +2,8 @@
 
 ASSISTANT_MAP = {
     "Mr. Aiken (Standard)": "asst_QvmAr0EQSkJbTz1egONsWRBy",  # Assistant ID
-     "Mr. Aiken (Difficult)": "asst_gcdfWZTrBXogpLqTsL5xMcKu"
+     "Mr. Aiken (Challenging)": "asst_gcdfWZTrBXogpLqTsL5xMcKu", 
+     "Mr. Smith (Standard)": "asst_Hx8eJQF0Noj0DHHdrXMqi3LP",
+     "Mr. Smith (Challenging)": "asst_9nLjlm3thGyGAU3dNTrDe5L2"
 }
  
