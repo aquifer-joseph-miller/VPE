12bdc331931ec7ea8f0098e3b65e53e35b9cc9d7
aquifer-joseph-miller <joseph.miller@aquifer.org>
2025-07-18 10:33:39 -0400

    Add files via upload

--- feedback_assistants.py
+++ feedback_assistants.py
@@ -4,7 +4,7 @@ FEEDBACK_ASSISTANTS = {
     "Mr. Aiken Feedback - Breadth": "asst_YsVDnU2LzQGRrym8cUKj1ZI5",
     "Mr. Smith Feedback": "asst_mfGdc7BVIVo6PSTQf6EicNiy", 
     "Mr. Smith Feedback - Breadth": "asst_mfGdc7BVIVo6PSTQf6EicNiy",
-    "Mrs. Kelly Feedback": "asst_8HCOI2YWOeqBejP68Z2FQP3A",
+    "Mrs. Kelly Feedback": "asst_VDMoRCzxDWfqiJnx4rGkOlE7",
     "Mrs. Kelly Feedback - Breadth": "asst_PYgRyEEVRohlODq2ENxmQnRw",
     "Depth Feedback": "asst_pG7EXYO2ajB7ZXx2NtNB7Sef", 
     "Relevance Feedback": "asst_pYiPS8q7gSmEXw07wgZy8x2T",
